6292ece0a2e8b942a98ced1a165987b45dec8792
Adrian <adrian@8-188.priv23.nus.edu.sg>
2015-10-16 15:40:39 +0800

    added sequence number and data length to header, works for corrupted filepath header

--- src/FileSender.java
+++ src/FileSender.java
@@ -8,6 +8,10 @@ import java.nio.file.Paths;
 import java.util.zip.*;
 
 public class FileSender {
+	
+	// Format of the packet
+	// <CRC>-------<sequence number>----<data length>----<data>
+	// <8bytes>----<4bytes>-------------<4bytes>---------<984bytes>
 
 	public static void main(String[] args) throws Exception 
 	{
@@ -19,13 +23,16 @@ public class FileSender {
 		// Create address to connect to
 		InetSocketAddress addr = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
 		
-		// Read in the file into a byte array
-		Path filepath = Paths.get(args[2]);
-		byte[] fileInBytes = Files.readAllBytes(filepath);
-		
 		// Create the socket to send packets
 		DatagramSocket socket = new DatagramSocket();
+		
+		// Create the CRC32 object
 		CRC32 crc = new CRC32();
+		
+		/* 
+		 * Create the filepath packet and sending it
+		 * ===========================================
+		 */
 		
 		// Read the destination string as a byte[]
 		byte[] destBytes = args[3].getBytes();
@@ -39,25 +46,65 @@ public class FileSender {
 		b.clear();
 		b.putLong(0);
 		
-		// Set the packet data
+		// Set the sequence number to 0 (the first packet sent)
+		b.putInt(0);
+		
+		// Set the number of data bytes
+		b.putInt(destBytes.length);
+		
+		// Set the data
 		b.put(destBytes);
 		
 		// Calculate and set the checksum in the packet header
 		crc.reset();
-		crc.update(data, 8, destBytes.length);
+		crc.update(data, Long.BYTES, data.length - Long.BYTES);
 		long checksum = crc.getValue();
 		b.rewind();
 		b.putLong(checksum);
 		
 		// Initialize the packet, destPath.length + 8 is likely < data.length(1000)
-		pathPacket = new DatagramPacket(data, destBytes.length + 8, addr);
+		pathPacket = new DatagramPacket(data, data.length, addr);
 		socket.send(pathPacket);
 		
 		// Debug message for filepath packet
+		System.out.println("Sending filepath packet.");
 		System.out.println("Sent CRC:" + checksum + " Contents:" + bytesToHex(data));
 		
+		/*
+		 * Check if the filepath packet is sent correctly
+		 * ================================================
+		 */
+		
+		boolean isCorrupted = true;
+		while (isCorrupted) {
+			
+			// After sending check for the ACK/NAK response from FileReceiver
+			byte[] response = new byte[1];
+			DatagramPacket responsePacket = new DatagramPacket(response, response.length);
+			socket.receive(responsePacket);
+			
+			// If ACK received, no resend required
+			if (response[0] == 1) {
+				System.out.println("Packet sent correctly.");
+				isCorrupted = false;
+			} else {
+				// NAK received, resend the filepath packet
+				System.out.println("Packet corrupted, resending packet.");
+				socket.send(pathPacket);
+			}
+		}
+		
+		/*
+		 * Sending the file as packets
+		 * =============================
+		 */
+		
+		// Read in the file into a byte array
+		Path filepath = Paths.get(args[2]);
+		byte[] fileInBytes = Files.readAllBytes(filepath);
+		
 		// Calculate the number of packets to send from the size of file
-		int numOfDataBytes = data.length - 8;
+		int numOfDataBytes = data.length - Long.BYTES - Integer.BYTES - Integer.BYTES;
 		int num = (int)Math.ceil(fileInBytes.length / (double)numOfDataBytes);
 		
 		// Reuse the data buffer created earlier
@@ -68,56 +115,48 @@ public class FileSender {
 		for (int i = 0; i < num; i++) {
 
 			// First assume no packet loss/corruption
-			
+			// Reserve space for the checksum
 			b.clear();
 			b.putLong(0);
+			
+			// Set the sequence number to i+1, given that 0 is reserved for filename
+			b.putInt(i + 1);
 
-			// Used for last packet instead of data
-			byte[] leftOver = null;
+			// Used to check if end of file byte[] is reached
 			int leftOverBytes;
 			
 			// Reached the end of the file with leftover bytes < 1000
 			if ((leftOverBytes = fileInBytes.length - i * numOfDataBytes) < numOfDataBytes) {
-				// Store the leftover bytes in a separate byte[]
-				leftOver = new byte[leftOverBytes + 8];
-				b = ByteBuffer.wrap(leftOver);
-				b.clear();
-				b.putLong(0);
+				
+				// Set the number of data bytes
+				b.putInt(leftOverBytes);
+				
+				// Set the data in the packet
 				b.put(fileInBytes, i * numOfDataBytes, leftOverBytes);
-				
-				// Set the checksum in the packet header
-				crc.reset();
-				crc.update(leftOver, 8, leftOver.length-8);
-				checksum = crc.getValue();
-				b.rewind();
-				b.putLong(checksum);
-				
-				// Initialize the packet
-				dataPacket = new DatagramPacket(leftOver, leftOver.length, addr);
 			} else {
+				
+				// Set the number of data bytes
+				b.putInt(numOfDataBytes);
+				
 				// General case, when the end of the file is not reached
 				b.put(fileInBytes, i * numOfDataBytes, numOfDataBytes);
-				
-				// Set the checksum in the packet header
-				crc.reset();
-				crc.update(data, 8, data.length-8);
-				checksum = crc.getValue();
-				b.rewind();
-				b.putLong(checksum);
-				
-				// Initialize the packet
-				dataPacket = new DatagramPacket(data, data.length, addr);
 			}
 	
+			// Set the checksum in the packet header
+			crc.reset();
+			crc.update(data, Long.BYTES, data.length - Long.BYTES);
+			checksum = crc.getValue();
+			b.rewind();
+			b.putLong(checksum);
+			
+			// Initialize the packet
+			dataPacket = new DatagramPacket(data, data.length, addr);
+			
 			// Send the packet
 			socket.send(dataPacket);
 			
 			// Debug message
-			if (leftOverBytes < numOfDataBytes) {
-				System.out.println("Sent CRC:" + checksum + " Contents:" + bytesToHex(leftOver));
-			} else {
-				System.out.println("Sent CRC:" + checksum + " Contents:" + bytesToHex(data));
-			}
+			//System.out.println("Sent CRC:" + checksum + " Contents:" + bytesToHex(data));
 		}
 	}
 	
